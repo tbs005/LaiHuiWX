1a8596ddb2ee532b762fc79396da88e0ef018cae
pzp_laihui <15738961936@.com>
2017-06-29 15:07:22 +0800

    初始上传

---
+++ src/main/java/com/lhpc/model/User.java
@@ -0,0 +1,85 @@
+package com.lhpc.model;
+
+import java.util.Date;
+
+public class User {
+    private Integer userId;
+
+    private String userMobile;
+
+    private String userName;
+
+    private String openId;
+
+    private Date createTime;
+
+    private Date loginTime;
+
+    private Double wallet;
+
+    private Integer userStatus;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserMobile() {
+        return userMobile;
+    }
+
+    public void setUserMobile(String userMobile) {
+        this.userMobile = userMobile == null ? null : userMobile.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId == null ? null : openId.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Date loginTime) {
+        this.loginTime = loginTime;
+    }
+
+    public Double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Double wallet) {
+        this.wallet = wallet;
+    }
+
+    public Integer getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(Integer userStatus) {
+        this.userStatus = userStatus;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" import="java.util.*" contentType="text/html; charset=utf-8"  %>  
+<html>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+ <meta http-equiv="Content-Language" content="ch-cn">
+<body>
+<h2>首页!</h2>
+</body>
+</html>
