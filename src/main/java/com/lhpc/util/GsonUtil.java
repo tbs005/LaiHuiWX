419b7edc4461741f3d19829312ebee5334433994
pzp_laihui <15738961936@.com>
2017-06-29 17:18:31 +0800

    返回数据类

--- src/main/java/com/lhpc/util/GsonUtil.java
+++ src/main/java/com/lhpc/util/GsonUtil.java
@@ -3,29 +3,47 @@ package com.lhpc.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import com.google.gson.Gson;
 
 public class GsonUtil {
-	
-	private GsonUtil(){
-		
+
+	private GsonUtil() {
+
 	}
-	
-	public static String getJson(int code,String message){
+
+	public static ResponseEntity<String> getJson(int code, String message) {
 		Gson gson = new Gson();
-		Map<String, Object> map = new HashMap<String,Object>();
+		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("code", code);
 		map.put("message", message);
-		return gson.toJson(map);
+		return new ResponseEntity<String>(gson.toJson(map), responseHeaders(),
+				HttpStatus.OK);
 	}
-	
-	public static String getJson(int code,String message,Object obj){
+
+	public static ResponseEntity<String> getJson(int code, String message,
+			Object obj) {
 		Gson gson = new Gson();
-		Map<String, Object> map = new HashMap<String,Object>();
+		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("code", code);
 		map.put("message", message);
 		map.put("data", obj);
-		return gson.toJson(map);
+		return new ResponseEntity<String>(gson.toJson(map), responseHeaders(),
+				HttpStatus.OK);
 	}
-	
+
+	/**
+	 * 响应头
+	 * 
+	 * @return
+	 */
+	public static HttpHeaders responseHeaders() {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Access-Control-Allow-Origin", "*");
+		responseHeaders.set("Content-Type", "application/json;charset=UTF-8");
+		return responseHeaders;
+	}
 }
--- src/main/java/com/lhpc/util/ParamVerificationUtil.java
+++ src/main/java/com/lhpc/util/ParamVerificationUtil.java
@@ -4,19 +4,46 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * 参数验证类
+ * 
  * @author pangzhenpeng
  *
  */
 public class ParamVerificationUtil {
-	private ParamVerificationUtil(){
-		
+	private ParamVerificationUtil() {
+
 	}
-	
+
 	/**
 	 * 发布行程参数验证
 	 */
-	public static boolean releaseItinerary(HttpServletRequest request){
-		
+	public static boolean releaseItinerary(HttpServletRequest request) {
+		boolean flag = false;
+		String startCity = request.getParameter("startCity");
+		String startCityCode = request.getParameter("startCityCode");
+		String startAddress = request.getParameter("startAddress");
+		String endCity = request.getParameter("endCity");
+		String endCityCode = request.getParameter("endCityCode");
+		String endAddress = request.getParameter("endAddress");
+		String price = request.getParameter("price");
+		String startTime = request.getParameter("startTime");
+		String carType = request.getParameter("carType");
+		String seats = request.getParameter("seats");
+		String strokeRoute = request.getParameter("strokeRoute");
+		if (StringUtil.isOrNotEmpty(startCity)
+				&& StringUtil.isOrNotEmpty(startAddress)
+				&& StringUtil.isOrNotEmpty(startCityCode)
+				&& StringUtil.isOrNotEmpty(endCity)
+				&& StringUtil.isOrNotEmpty(endCityCode)
+				&& StringUtil.isOrNotEmpty(endAddress)
+				&& StringUtil.isOrNotEmpty(price)
+				&& StringUtil.isOrNotEmpty(startTime)
+				&& StringUtil.isOrNotEmpty(carType)
+				&& StringUtil.isOrNotEmpty(seats)
+				&& StringUtil.isOrNotEmpty(strokeRoute)) {
+			flag = true;
+
+		}
+		return flag;
 	}
 
 }
