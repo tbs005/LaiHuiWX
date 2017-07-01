3b05c621722ea54f970a41c381542cbee60d7c00
AnYangWuYanZu <842154692@qq.com>
2017-07-01 15:33:10 +0800

    注册登录

--- src/main/java/com/lhpc/util/ConfigUtil.java
+++ src/main/java/com/lhpc/util/ConfigUtil.java
@@ -1,5 +1,9 @@
 package com.lhpc.util;
 
 public class ConfigUtil {
+	
+	public static final String WX_APP_ID = "wxd6d79c4ca0fef838";//公众号id
+	
+	public static final String WX_SECRET_KEY = "bace843875970ae9a941dcb6dbe16e6d";//公众号秘钥
 
 }
