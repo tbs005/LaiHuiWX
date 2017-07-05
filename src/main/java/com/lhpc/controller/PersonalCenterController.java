56ff3e6b5fc1e6cd0105326534480a363006959b
AnYangWuYanZu <842154692@qq.com>
2017-07-05 09:34:23 +0800

    no message

--- src/main/java/com/lhpc/controller/PersonalCenterController.java
+++ src/main/java/com/lhpc/controller/PersonalCenterController.java
@@ -1,10 +1,37 @@
 package com.lhpc.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lhpc.util.GsonUtil;
+import com.lhpc.util.ResponseCodeUtil;
+
 /**
  * 个人中心controller
  * @author YangGuang
  *
  */
+@Controller
+@RequestMapping(value="/wx")
 public class PersonalCenterController {
+	
+	private static Logger log = Logger.getLogger(PersonalCenterController.class);
 
+	/**
+	 * 个人中心我的行程
+	 */
+	@ResponseBody
+	@RequestMapping(value="personal/itinerary")
+	public ResponseEntity<String> personalItinerary() {
+		try {
+				return GsonUtil.getJson(ResponseCodeUtil.SUCCESS, "请求成功");
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return GsonUtil.getJson(ResponseCodeUtil.SYSTEM_ERROR, "服务器繁忙,请重试!");
+		}
+		
+	}
 }
