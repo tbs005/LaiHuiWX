83d17bbedb99e31c06b3f1acc7fa4aea902c1e07
pzp_laihui <15738961936@.com>
2017-07-08 14:05:56 +0800

    no message

--- src/main/java/com/lhpc/util/ResponseCodeUtil.java
+++ src/main/java/com/lhpc/util/ResponseCodeUtil.java
@@ -22,8 +22,10 @@ public class ResponseCodeUtil {
 	public static final int SEAT_LACK = 4101; // 车主座位不足
 	public static final int PUSH_ERROR = 4102; // 推送失败
 	public static final int FLUSH_TOKEN_ERROR = 4103; // 刷新失败
+	public static final int PAY_NOTIFY_ERROR = 4201;// 系统错误
 	public static final int ERROR = 5000;// 操作失败
 	public static final int SYSTEM_ERROR = 5005;// 系统错误
+	
 	
 
 }
